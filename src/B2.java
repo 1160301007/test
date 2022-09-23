caeb55967fd15dfc9db036e03eb9824815f163cb
zhaonan <zhouzhaonan1997@gmail.com>
2022-09-23 14:19:32 +0800

    addforget

--- src/B2.java
+++ src/B2.java
@@ -1,2 +1,4 @@
 public class B2 {
+    int commit1;
+
 }
--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,6 @@ public class Main {
     public static void main(String[] args) {
         int a = 103;
         System.out.println("Hello world!");
+        int forget;
     }
 }
